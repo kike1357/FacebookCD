cde9d5b581657dcde28b6b3009154773789af8df
pastoriza57 <pastoriza57@pastoriza57-PC>
2016-06-20 01:17:36 +0200

    subida

---
+++ src/facebookcd/FacebookCD.java
@@ -0,0 +1,12 @@
+package facebookcd;
+
+
+public class FacebookCD {
+
+    
+    public static void main(String[] args) {
+        Ventana obx = new Ventana();
+        obx.setVisible(true);
+    }
+    
+}
